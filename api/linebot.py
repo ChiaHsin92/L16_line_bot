b3d95955dc7bb8d5f97f574d544b772c7860671d
ChiaHsin92 <146929015+ChiaHsin92@users.noreply.github.com>
2025-04-18 23:24:16 +0800

    linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -593,8 +593,6 @@ def handle_message(event):
         except Exception as e:
             logger.error(f"場地詳情查詢失敗：{e}", exc_info=True)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"⚠ 發生錯誤：{e}"))
-             
- 
- 
- if __name__ == "__main__":
-     app.run()
+
+if __name__ == "__main__":
+    app.run()
