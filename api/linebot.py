3fdd828be5ef851a39c2a744f0ce00df685d480f
ChiaHsin92 <146929015+ChiaHsin92@users.noreply.github.com>
2025-04-28 21:21:53 +0800

    linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -829,7 +829,7 @@ def handle_message(event):
                 }
             }
         )
-        line_bot_api.reply_message(event.reply_token, flex_message
+        line_bot_api.reply_message(event.reply_token, flex_message)
 
     
     else:
